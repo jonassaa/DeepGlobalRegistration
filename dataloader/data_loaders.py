8b07a93a4402f010c4fc14d983e362d4e0a659f4
Jonas Strand Aasberg <jonassaa@stud.ntnu.no>
2022-06-03 11:29:06 +0200

    adding zivid dataset and dataloader

--- dataloader/data_loaders.py
+++ dataloader/data_loaders.py
@@ -6,10 +6,11 @@
 # - Christopher Choy, JunYoung Gwak, Silvio Savarese, 4D Spatio-Temporal ConvNets: Minkowski Convolutional Neural Networks, CVPR 2019
 from dataloader.threedmatch_loader import *
 from dataloader.kitti_loader import *
+from dataloader.zivid_loader import *
 
 ALL_DATASETS = [
     ThreeDMatchPairDataset07, ThreeDMatchPairDataset05, ThreeDMatchPairDataset03,
-    ThreeDMatchTrajectoryDataset, KITTIPairDataset, KITTINMPairDataset
+    ThreeDMatchTrajectoryDataset, KITTIPairDataset, KITTINMPairDataset, ZividPairDataset
 ]
 dataset_str_mapping = {d.__name__: d for d in ALL_DATASETS}
 
